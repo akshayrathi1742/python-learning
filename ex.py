cc824eb39f3c8194d700cbd86805d62092d8c395
akshayrathi1742 <akshayrathi1742@gmail.com>
2022-04-14 00:30:54 +0530

    commit

---
+++ ex.py
@@ -0,0 +1,72 @@
+def getdate():
+    import datetime
+    return datetime.datetime.now()
+def take (k):
+    if k==1:
+        c =int (input("enter 1 for excersie and 2 for food"))
+        if (c==1):
+            value = input ("type here\n")
+            with open("akshay.txt","a")as op:
+                op.write(str([str(getdate())])+": "+value+"\n")
+            print("successfully written")
+        elif(c==2):
+            value = input("type here\n")
+            with open ("akshay.txt","a")as op:
+                op.write(str([str(getdate())])+": "+value+"\n")   
+    elif(k==2):
+        c =int (input("enter 1 for excersie and 2 for food"))
+        if (c == 1):
+            value = input("type here\n")
+            with open("rohan.txt","a")as op:
+                op.write(str([str(getdate())])+": "+value+"\n")  
+            print("successfully written")
+        elif (c==2):
+            value = input("type here\n")
+            with open("rohan.txt","a") as op:
+                op.write(str([str(getdate())])+": "+value+"\n")
+            print("successfully written") 
+    elif (k==3):
+        c =int (input("enter 1 for excersie and 2 for food"))
+        if (c==1):
+            value = input("type here\n")
+            with open("haamad.txt","a")as op:
+               op.write(str([str(getdate())])+": "+value+"\n") 
+            print("successfully written")
+        else:
+            print("plz enter valid input (1(akshay),2(rohan),3(haamad))")
+def retrieve(k):
+    if k==1:
+        c =int (input("enter 1 for excersie and 2 for food"))
+        if (c==1):
+            with open("akshay.txt")as op:
+                for i in op:
+                    print(i,end = "")
+        elif (c==2):
+            for i in op:
+                print(i, end = "")
+    elif (k==2):
+        c =int (input("enter 1 for excersie and 2 for food"))
+        if (c== 1):
+            with open ("rohan.txt",)as op:
+                for i in op:
+                    print(i, end ="")
+        elif (c == 2):
+            with open("haamad.txt")as op:
+                for i in op:
+                    print(i, end="")
+    else:
+        print("plz enter vaild input (akshay,rohan,haamad)")
+print("health managemanent system:")
+a = int(input("press 1 for log the value and 2 for retrieve"))
+
+if a==1:
+    b = int(input("press 1 for akshay press 2 for rohan 3 press for haamad "))
+    take (b)
+else:
+    b = int(input("press 1 for akshay press 2 for rohan 3 press for haamad "))
+    retrieve(b) 
+    
+    
+        
+                            
+                                     
--- filereading.py
+++ filereading.py
@@ -1,9 +1,13 @@
-f = open("Bugatti.txt","rt")
+f = open(
+    "Bugatti.txt","rt"
+    )
 # print(f.readlines())
 # print(f.readline())
 # content = f.read()
 for line in f:
-    print(line,end="")
+    print(
+        line,end=""
+        )
 # print(content)
 # content = f.read(20)
 # print(content)
