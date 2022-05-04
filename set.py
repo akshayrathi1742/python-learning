f3c51be0349efd58d2398cd478ff53357d11fe9c
Akshay rathi <akshayrathi1742@gmail.com>
2022-05-04 15:26:29 +0530

    commit

---
+++ set.py
@@ -0,0 +1,22 @@
+# set = { 8 ,'ram','sam','rahul','robin'}
+# set.add('ram')
+# set.add('kamal')
+# # print(set)
+# set_2 = {1,2,3,4,5,'rahul'}
+# # print(set.difference(set_2))
+# # print(set_2.discard('rahul'))
+# # print(set_2)
+# # print(set.difference_update(set_2))
+# # print(set)
+# set_3 = set_2.copy()
+# print(set_3)
+# list = [1,2,3,4,5,5]
+# print(list[1::2])
+set = {1,2,3,4,5}
+set_2 ={4,5,6,7,8,9 }
+# print(set.issubset(set_2))
+# print(set.union(set_2))
+print(set.intersection(set_2))
+# print(set_2.difference_update(set))
+# print(set_2)
+# print(set)
---
+++ tuple.py
@@ -0,0 +1,11 @@
+tuple =(1,2,3,4,5,5)
+# 
+print(tuple.count(5))
+
+my_tuple = tuple[1:4] # for indexing
+print(my_tuple)
+
+a,b,c,*other = (1,2,3,4,5)# it change a tuple in list
+print(a,b,c)
+
+
